11f402442ae4001f31ead95a61d1ced703015abc
Hiếu Lê <leminhhieu2511@gmail.com>
2023-09-28 04:47:48 +0700

    edit code

--- function/ServiceBusQueueTrigger/__init__.py
+++ function/ServiceBusQueueTrigger/__init__.py
@@ -3,20 +3,21 @@ import azure.functions as func
 import psycopg2
 import os
 from datetime import datetime
-from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import Mail
+# from sendgrid import SendGridAPIClient
+# from sendgrid.helpers.mail import Mail
+import smtplib
+from socket import gaierror
 
 def main(msg: func.ServiceBusMessage):
-
-    SENDGRID_API_KEY = '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'
+    print('msg.getBody:', msg.get_body())
     notification_id = int(msg.get_body().decode('utf-8'))
     logging.info('Python ServiceBus queue trigger processed message: %s',notification_id)
 
     # Get connection to database
-    connection = psycopg2.connect(host="techconf-db-server15.postgres.database.azure.com", user="azureuser@techconf-db-server15", 
-                                password="P@ssword123456", database="techconfdb")
+    connection = psycopg2.connect(host="maitp1-server.postgres.database.azure.com", user="adminadmin@maitp1-server", 
+                                password="Hieule~~2511", database="techconfdb")
     cur = connection.cursor()
-    sg = SendGridAPIClient(SENDGRID_API_KEY)
+
     try:
         # Get notification message and subject from database using the notification_id
         cur.execute("Select subject, message from notification where id = {}".format(notification_id))
@@ -26,11 +27,21 @@ def main(msg: func.ServiceBusMessage):
         cur.execute("Select email, last_name, first_name from attendee")
         attendees = cur.fetchall()
 
+        ##client = mt.MailtrapClient(token="e1e4a588a0278984b34c47d578eef84a")
+        sender = "nhoxkho25111998@gmail.com"
         # Loop through each attendee and send an email with a personalized subject
         for attendee in attendees:
-            mail = Mail(from_email='haitt.dev15@gmail.com', to_emails= attendee[0], subject= notification[0], 
-            plain_text_content= "Hi {}, \n {}".format(attendee[2], attendee[1], notification[1]))
-            sg.send(mail)
+            text="Hi {} {}, \n {}".format(attendee[2], attendee[1], notification[1])
+                                       
+            message = f"""\
+            Subject: {notification[0]}
+            To: {attendee[0]}
+            From: {sender}
+            {text}"""
+
+            with smtplib.SMTP("sandbox.smtp.mailtrap.io", 2525) as server:
+                server.login("00b4c8b6d56518", "067dd2b67c4da3")
+                server.sendmail(sender, attendee[0], message)
         
         status = "Notified {} attendees".format(len(attendees))
         # Update the notification table by setting the completed date and updating the status with the total number of attendees notified
@@ -39,6 +50,7 @@ def main(msg: func.ServiceBusMessage):
     except (Exception, psycopg2.DatabaseError) as error:
         logging.error(error)
         connection.rollback()
+        print('Sent')
     finally:
         # Close connection
         cur.close()
--- web/config.py
+++ web/config.py
@@ -4,15 +4,15 @@ app_dir = os.path.abspath(os.path.dirname(__file__))
 
 class BaseConfig:
     DEBUG = True
-    POSTGRES_URL="techconf-db-server15.postgres.database.azure.com"
-    POSTGRES_USER="azureuser@techconf-db-server15"
-    POSTGRES_PW="P@ssword123456"
+    POSTGRES_URL="maitp1-server.postgres.database.azure.com"
+    POSTGRES_USER="adminadmin@maitp1-server"
+    POSTGRES_PW="Hieule~~2511"
     POSTGRES_DB="techconfdb"
     DB_URL = 'postgresql://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI') or DB_URL
     CONFERENCE_ID = 1
     SECRET_KEY = 'LWd2tzlprdGHCIPHTd4tp5SBFgDszm'
-    SERVICE_BUS_CONNECTION_STRING ='Endpoint=sb://techconfservicebus.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=kTVpMwXoN5m43MvlcGnLufkTB8Zj6d94OdgopR86mD0='
+    SERVICE_BUS_CONNECTION_STRING ='Endpoint=sb://maitp1-pj3-service-bus.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=+L5BI4WY/HrvJzhf6bLLnJkKS+ibMZDHD+ASbEP2xP0='
     SERVICE_BUS_QUEUE_NAME ='notificationqueue'
     ADMIN_EMAIL_ADDRESS: 'info@techconf.com'
     SENDGRID_API_KEY = '' #Configuration not required, required SendGrid Account
